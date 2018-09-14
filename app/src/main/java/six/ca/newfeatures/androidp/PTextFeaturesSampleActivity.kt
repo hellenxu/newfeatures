ee5788796d80e1bab772f6c4d95312261d1e6c9b
hellenxu <heavensxu@gmail.com>
2018-09-13 22:28:58 -0400

    Add: TextLinks example.

--- app/src/main/java/six/ca/newfeatures/androidp/PTextFeaturesSampleActivity.kt
+++ app/src/main/java/six/ca/newfeatures/androidp/PTextFeaturesSampleActivity.kt
@@ -2,10 +2,13 @@ package six.ca.newfeatures.androidp
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.SpannableString
+import android.view.textclassifier.TextClassifier
+import android.view.textclassifier.TextLinks
 import android.widget.TextView
 import androidx.core.text.PrecomputedTextCompat
 import androidx.core.widget.TextViewCompat
-import kotlinx.android.synthetic.main.act_precomputed.*
+import kotlinx.android.synthetic.main.act_text_features.*
 import six.ca.newfeatures.R
 import java.lang.ref.WeakReference
 import java.util.concurrent.Executor
@@ -15,12 +18,18 @@ import java.util.concurrent.Executors
  * @CopyRight six.ca
  * Created by Heavens on 2018-09-07.
  */
-class PrecomputedTextSampleActivity: Activity() {
+class PTextFeaturesSampleActivity: Activity() {
+    private val textClassifier: TextClassifier by lazy {
+        object: TextClassifier{}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.act_precomputed)
+        setContentView(R.layout.act_text_features)
+
         asyncSetText(computedText, getString(R.string.long_string), executor = Executors.newSingleThreadExecutor())
+        generateLinks(smartLinkifyText, executor = Executors.newSingleThreadExecutor())
+
     }
 
     private fun asyncSetText(textSource: TextView, longString: String, executor: Executor) {
@@ -35,6 +44,21 @@ class PrecomputedTextSampleActivity: Activity() {
             val text = PrecomputedTextCompat.create(longString, params)
             textViewOut.post {
                 textSource.text = text
+            }
+        }
+    }
+
+    private fun generateLinks(textSource: TextView, executor: Executor) {
+        val textSpannable = SpannableString(textSource.text)
+        val request = TextLinks.Request.Builder(textSpannable).build()
+        val ref = WeakReference<TextView>(textSource)
+
+        executor.execute {
+            val textView = ref.get() ?: return@execute
+
+            textClassifier.generateLinks(request).apply(textSpannable, TextLinks.APPLY_STRATEGY_REPLACE, null)
+            textView.post {
+                textView.text = textSpannable
             }
         }
     }
