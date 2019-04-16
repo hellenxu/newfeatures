28300adb67f7d3225a2dc2cc677f9207d243c4f1
hellenxu <heavensxu@gmail.com>
2019-04-15 21:58:36 -0400

    Add: MotionLayout example.

---
+++ app/src/main/java/six/ca/newfeatures/layout/MotionLayoutSample.kt
@@ -0,0 +1,17 @@
+package six.ca.newfeatures.layout
+
+import android.app.Activity
+import android.os.Bundle
+import six.ca.newfeatures.R
+
+/**
+ * @CopyRight six.ca
+ * Created by Heavens on 2019-04-15.
+ */
+class MotionLayoutSample: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_motion)
+    }
+}
