e8a03c7d9948cbd1514a24ac4b85bf0be1f48d98
hellenxu <heavensxu@gmail.com>
2019-05-14 22:35:46 -0400

    Modified: clean up.

--- app/src/main/java/six/ca/newfeatures/androidp/WebViewSample.kt
+++ app/src/main/java/six/ca/newfeatures/androidp/WebViewSample.kt
@@ -2,8 +2,6 @@ package six.ca.newfeatures.androidp
 
 import android.app.Activity
 import android.os.Bundle
-import android.webkit.ValueCallback
-import android.webkit.WebSettings
 import android.webkit.WebView
 import kotlinx.android.synthetic.main.act_wv.*
 import six.ca.newfeatures.R
