9d7a2cc62939d60b760922c7f3c18c28e177c0da
hellenxu <heavensxu@gmail.com>
2019-05-14 22:32:17 -0400

    Added: enable safe browsing example.

---
+++ app/src/main/java/six/ca/newfeatures/androidp/WebViewSample.kt
@@ -0,0 +1,28 @@
+package six.ca.newfeatures.androidp
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebSettings
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.act_wv.*
+import six.ca.newfeatures.R
+
+/**
+ * @CopyRight six.ca
+ * Created by Heavens on 2019-05-14.
+ */
+class WebViewSample: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_wv)
+
+        wv.settings.safeBrowsingEnabled = true
+        WebView.startSafeBrowsing(this, object: ValueCallback<Boolean> {
+            override fun onReceiveValue(value: Boolean?) {
+                if (value == true)println("xxl-safe browsing on") else println("xxl-safe browsing off")
+            }
+        })
+    }
+}
