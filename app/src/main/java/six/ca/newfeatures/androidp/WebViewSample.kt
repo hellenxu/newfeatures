23d8e6cb216d016ea1d5eaa079b6386c2061096f
hellenxu <heavensxu@gmail.com>
2019-05-14 22:35:06 -0400

    Added: whitelist example.

--- app/src/main/java/six/ca/newfeatures/androidp/WebViewSample.kt
+++ app/src/main/java/six/ca/newfeatures/androidp/WebViewSample.kt
@@ -12,17 +12,21 @@ import six.ca.newfeatures.R
  * @CopyRight six.ca
  * Created by Heavens on 2019-05-14.
  */
-class WebViewSample: Activity() {
+class WebViewSample : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_wv)
 
         wv.settings.safeBrowsingEnabled = true
-        WebView.startSafeBrowsing(this, object: ValueCallback<Boolean> {
-            override fun onReceiveValue(value: Boolean?) {
-                if (value == true)println("xxl-safe browsing on") else println("xxl-safe browsing off")
-            }
-        })
+        WebView.startSafeBrowsing(this) { value ->
+            if (value == true) println("xxl-safe browsing on") else println("xxl-safe browsing off")
+        }
+
+        //set white list
+        val array = ArrayList<String>()
+        array.add("abc.com")
+        WebView.setSafeBrowsingWhitelist(array) { }
+
     }
 }
