180de0f5794b59e20fbaf61c54061ebaab6abae4
hellenxu <heavensxu@gmail.com>
2019-05-27 21:47:52 -0400

    Added: vp2 part one.

---
+++ app/src/main/java/six/ca/newfeatures/jetpack/ViewPager2Sample.kt
@@ -0,0 +1,19 @@
+package six.ca.newfeatures.jetpack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import six.ca.newfeatures.R
+
+/**
+ * @author hellenxu
+ * @date 2019-05-27
+ * Copyright 2019 Six. All rights reserved.
+ */
+class ViewPager2Sample: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_vp)
+
+    }
+}
